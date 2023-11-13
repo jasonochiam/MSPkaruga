bb0d7aa7ff0b01c4a36e03ac60a55e97591fc6de
Aidan Aalund <aidanaalund@icloud.com>
2023-11-13 16:58:01 -0600

    Added TODOs for Switch.c

--- ECE319K_Lab9/Switch.c
+++ ECE319K_Lab9/Switch.c
@@ -9,11 +9,18 @@
 // LaunchPad.h defines all the indices into the PINCM table
 void Switch_Init(void){
     // write this
-
+    // TODO: change the indexes to reflect our switches
+    IOMUX->SECCFG.PINCM[PB0INDEX] = 0x00100081;
+    IOMUX->SECCFG.PINCM[PB1INDEX] = 0x00100081;
+    IOMUX->SECCFG.PINCM[PB2INDEX] = 0x00100081;
+    IOMUX->SECCFG.PINCM[PB6INDEX] = 0x00100081;
+    IOMUX->SECCFG.PINCM[PB7INDEX] = 0x00100081;
 }
 // return current state of switches
 uint32_t Switch_In(void){
     // write this
 
-  return 0; //replace this your code
+  return GPIOA->DIN31_0; //replace this your code
 }
+
+// TODO: write a version of Switch_In that masks the 5 pins we use as switches
